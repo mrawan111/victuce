144e3989a6291bd38c0383694df5918b97c67796
AmenHaitham <aminmonmon3@gmail.com>
2025-12-21 12:59:45 +0200

    d

--- src/main/java/com/victusstore/controller/HealthController.java
+++ src/main/java/com/victusstore/controller/HealthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.sql.DataSource;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.time.Duration;
 import java.time.Instant;
