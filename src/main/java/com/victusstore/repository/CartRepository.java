891a630979b3a94d4f48a7520b47402456a7879d
mrawan111 <mrawanamr5@gmail.com>
2026-02-25 22:53:52 +0200

    solve cart warning

--- src/main/java/com/victusstore/repository/CartRepository.java
+++ src/main/java/com/victusstore/repository/CartRepository.java
@@ -3,9 +3,11 @@ package com.victusstore.repository;
 import com.victusstore.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByAccount_Email(String email);
+    List<Cart> findAllByAccount_Email(String email);
 }
